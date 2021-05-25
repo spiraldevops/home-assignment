beda46cbec96d33f41d3381151fd30156587a5c8
vodwood <vodwood@gmail.com>
2021-05-25 18:15:38 +0300

    add

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# home-assignment
+## services (containers)
+All applications require python 3.6 runtime.
+
+The application services are:
+
+### names
+entrypoint: server.py
+expects a GET request, returns a random name in the body.
+listens on port 8000 by default
+
+example:
+```bash
+curl localhost:8000
+```
+
+returns:
+```<html><body><h1>Mary Jackson</h1></body></html>```
+
+### numbers 
+
+entrypoint: server.py
+expects a GET request, returns a random number in the body
+listens on port 8000 by default
+
+example:
+```bash
+curl localhost:8000
+```
+
+returns:
+```<html><body><h1>107</h1></body></html>```
+
+### content
+entrypoint server.py
+expects a GET request, returns the list of customer names from DB
+listens on port 8000 by default
+
+required the following ENV VARS to configure the DB connection in the container:
+MYSQL_HOST
+MYSQL_USER
+MYSQL_PASSWORD
+MYSQL_DATABASE
+
+requires the DB to have a table names customers with a column named "names" (see mysql dev content deployment dump instructions)
+
+### scale
+entrypoint: server.py
+Expects a POST request with a number between 1 and 100 in the body (curl -X POST -d 54 localhost:8000)
+Loads all cores on the server to the percentage specified in the POST body
+
+example:
+```bash
+curl -X POST -d 54 localhost:8000
+```
+
+returns:
+```<html><body><h1>Loading CPU to 54 percent</h1></body></html>```
+
+
+## Database schema and configuration
+
+The database required is a mysql 5.7 with InnoDB engine
+
+The database schema is very simple
+The db should be called "customers"
+It consists of a single table called "customers" with the following columns:
+
+	name VARCHAR(100)
+	debt INT
+
+A db dump file for import is in the repo root (customers.sql)
+Can be used directly for import or reference
---
+++ scale/server.py
@@ -0,0 +1,66 @@
+import argparse
+import numbers
+import CPULoadGenerator
+import psutil
+import os
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+    def _html(self, message):
+        """This just generates an HTML document that includes `message`
+        in the body. Override, or re-write this do do more interesting stuff.
+        """
+        content = f"<html><body><h1>{message}</h1></body></html>"
+        return content.encode("utf8")  # NOTE: must return a bytes object!
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write(self._html("hi!"))
+
+    def do_HEAD(self):
+        self._set_headers()
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        pd = post_data.decode("utf-8")
+        pc = float(pd) / 100
+        self._set_headers()
+        if (pd.isnumeric()):
+            self.wfile.write(self._html("Loading CPU to {} percent".format(pd)))
+            os.system("python CPULoadGenerator.py -l {}".format(pc))
+        else:
+            self.wfile.write(self._html("Please provide a number between 0 and 100"))
+
+def run(server_class=HTTPServer, handler_class=S, addr="localhost", port=8000):
+    server_address = (addr, port)
+    httpd = server_class(server_address, handler_class)
+
+    print(f"Starting httpd server on {addr}:{port}")
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Run a simple HTTP server")
+    parser.add_argument(
+        "-l",
+        "--listen",
+        default="0.0.0.0",
+        help="Specify the IP address on which the server listens",
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        default=8000,
+        help="Specify the port on which the server listens",
+    )
+    args = parser.parse_args()
+    run(addr=args.listen, port=args.port)
